a79a5efed9eac12e1ce02f154293bf4fc3e0ac23
shimonsw <shimi0007@hotmail.com>
2015-05-14 22:31:57 +0300

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-/**
- * Created by swisa on 14/05/2015.
- */
+var express    = require("express");
+var app        = express();
+var server     = require('http').createServer(app);
+var path       = require('path');
+var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// use middleware
+app.use(express.static(path.join(__dirname, 'includes')));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// define routes
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+});
+
+mongoose.connect('mongodb://localhost:27017/mongo');
+
+app.use(require('./routers'));
+
+var port = process.env.PORT || 3000;
+server.listen(port, function(){
+    console.log("app http ready on port "+port);
+});
